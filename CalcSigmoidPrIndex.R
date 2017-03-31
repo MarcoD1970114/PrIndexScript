dc4a8e5070ee93f1ad765909bf22251a8b5ec1c7
MarcoD1970114 <marco.dumont@gmail.com>
2017-03-31 12:45:47 +0200

    selection and dataframe with data needed

--- CalcSigmoidPrIndex.R
+++ CalcSigmoidPrIndex.R
@@ -1,3 +1,7 @@
+
+
+
+
 library(jsonlite)
 library(stringr)
 
@@ -16,7 +20,8 @@ url <-
 # read url and convert to data.frame
 document <- fromJSON(txt = url)
 
-number = document$response$numFound
+#number = document$response$numFound
+number  = 1000
 start = 0
 
 investigated = 0
@@ -26,42 +31,46 @@ investigated = 0
 print(paste('number of libraries: ', number))
 
 while (number > start) {
-  url <- URLencode(
-    paste(
-      sep = ''
-      ,
-      'http://search.maven.org/solrsearch/select?q=l:"sources"+AND+p:"jar"&rows=10&start=',
-      start
+  tryCatch({
+    url <- URLencode(
+      paste(
+        sep = ''
+        ,
+        'http://search.maven.org/solrsearch/select?q=l:"sources"+AND+p:"jar"&rows=100&start=',
+        start
+      )
     )
-  )
-  urldocument <- fromJSON(txt = url)
+    urldocument <- fromJSON(txt = url)
+  }, error=function(e){})
   #read them all, this may take a while.
   
-  start = start + 10
+  start = start + 100
   
   print(paste('loaded libraries: ', start))
   
   for (mavenlib in rows(urldocument$response$docs)) {
     #query versions
-    url <-
-      URLencode(
-        paste(
-          sep = '',
-          'http://search.maven.org/solrsearch/select?q=g:"',
-          mavenlib$g,
-          '"+AND+a:"'
-          ,
-          mavenlib$a,
-          '"&core=gav&rows=100'
+    tryCatch({
+      url <-
+        URLencode(
+          paste(
+            sep = '',
+            'http://search.maven.org/solrsearch/select?q=g:"',
+            mavenlib$g,
+            '"+AND+a:"'
+            ,
+            mavenlib$a,
+            '"&core=gav&rows=100'
+          )
         )
-      )
-    
-    versions <- fromJSON(txt = url)
+      
+      print(url)
+      
+      versions <- fromJSON(txt = url)
+    }, error=function(e){ })
+   # Sys.sleep(1)
     
     if (length(rows(versions$response$docs)) > 40) {
-      versions$response$ordereddocs <-
-        versions$response$docs[with(versions$response$docs, order(v)), ]
-      
       majors = c()
       
       
@@ -70,55 +79,71 @@ while (number > start) {
         if (str_detect(version$v, "\\.") > 0) {
           major <- str_split(version$v, "\\.")[[1]]
           #print (paste(major[1], "\\", version$v))
-          if (!length(which(majors == major[1])) > 0) {
-            majors = c(majors, c(major[1]))
+          if (!is.na(suppressWarnings(as.numeric(major[1])))) {
+            if (!length(which(majors == major[1])) > 0) {
+              majors = c(majors, c(major[1]))
+            }
           }
         }
         # add to list
       }
       
-      print(majors)
+      majors = sort(majors, decreasing = FALSE)
+      #print(majors)
       
       if (length(majors) >= 3) {
         #loop thru second to n-1 majors
         
         print ('found >3 majors')
+        points2 = c()
+        points3 = c()
+        urls = c()
+        PrIndex = c()
+        id = c()
+        top = c()
         
-        for (major in majors) {
-        for (version in rows(versions$response$docs)) {
-            
+        for (major in majors[1:length(majors) - 1]) {
+          #skip the first and the last.
           
-         
           
-          a = str_replace_all(version$a, "\\.", "/")
-          g = str_replace_all(version$g, '\\.', '/')
-          
-          path <-
-            paste(
-              sep = '',
-              'http://search.maven.org/remotecontent?filepath=',
-              g
-              ,
-              '/',
-              a,
-              '/',
-              version$v,
-              '/',
-              version$a,
-              '-',
-              version$v,
-              '-sources.jar'
-            )
-          
-          #print(paste('loading source', path))
-          
-          #curl::curl(url=path )
+          for (version in rows(versions$response$docs)) {
+            if (startsWith(version$v, paste(sep = "", major, '.'))) {
+              a = str_replace_all(version$a, "\\.", "/")
+              g = str_replace_all(version$g, '\\.', '/')
+              
+              path <-
+                paste(
+                  sep = '',
+                  'http://search.maven.org/remotecontent?filepath=',
+                  g
+                  ,
+                  '/',
+                  a,
+                  '/',
+                  version$v,
+                  '/',
+                  version$a,
+                  '-',
+                  version$v,
+                  '-sources.jar'
+                )
+              
+              #print(paste(major,  version$v))
+              
+ 
+              points2 = c(points2, str_split(version$v, "\\.")[[1]][2])
+              points3 = c(points3, str_split(version$v, "\\.")[[1]][3])
+              urls = c(urls, path)
+              id = c(id, version$id)
+              top = c(top, major)
+              
+            }
+          }
         }
-        }
+        df = data.frame(id, urls, top, points2, points3)
       }
     } else {
       #print (paste ('versions', length(rows(versions$response$docs))))
     }
   }
-  
 }
